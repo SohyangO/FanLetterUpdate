5af802ec7788b120c040ba995884eb098120aef4
SohyangO <shyang903@naver.com>
2024-02-20 10:09:20 +0900

    add:Router 셋업

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "pages/home";
 import Detail from "pages/detail";
+import Login from "pages/Login";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="detail/:id" element={<Detail />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
