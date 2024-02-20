701d02ab0c03d02e40635951074d82269199bf9c
SohyangO <shyang903@naver.com>
2024-02-20 18:03:21 +0900

    UPDATE: 로그인, 회원가입 버튼 토글 기능 완료

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleClickLogin = () => {
+    navigate("/Login");
+  };
   return (
     <>
       <header>
         <h1>home</h1>
         <nav>내 프로필</nav>
-        <nav>로그인</nav>
+        <button onClick={handleClickLogin}>로그인</button>
       </header>
     </>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const [isLoginForm, setIsLoginForm] = useState(true);
+
+  const renderLoginForm = () => {
+    return (
+      <>
+        <div>
+          <form action="">
+            <input
+              type="text"
+              placeholder="아이디 (4~10글자)"
+              minLength={4}
+              maxLength={10}
+            />
+            <input
+              type="password "
+              placeholder="비밀번호 (4~15글자)"
+              minLength={4}
+              maxLength={15}
+            />
+          </form>
+        </div>
+      </>
+    );
+  };
+
+  const renderSignUpForm = () => {
+    return (
+      <>
+        <div>
+          <form action="">
+            <input
+              type="text"
+              placeholder="닉네임(1~10글자)"
+              minLength={1}
+              maxLength={10}
+            />
+            <input
+              type="text"
+              placeholder="아이디 (4~10글자)"
+              minLength={4}
+              maxLength={10}
+            />
+            <input
+              type="password "
+              placeholder="비밀번호 (4~15글자)"
+              minLength={4}
+              maxLength={15}
+            />
+          </form>
+        </div>
+      </>
+    );
+  };
+
+  const toggleForm = () => {
+    setIsLoginForm(!isLoginForm);
+  };
+
+  return (
+    <>
+      <div>
+        <h2>{isLoginForm ? "로그인" : "회원가입"}</h2>
+        {isLoginForm ? renderLoginForm() : renderSignUpForm()}
+        <button onClick={toggleForm}>
+          {isLoginForm ? "회원가입" : "로그인"}
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Login;
